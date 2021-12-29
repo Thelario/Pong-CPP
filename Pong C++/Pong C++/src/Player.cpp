533c5c7b3709dbda9e84604a6233681daac21cb9
Pablo Lario Gómez <62502964+Thelario@users.noreply.github.com>
2021-12-29 19:44:36 +0100

    Player Movement in y axis

--- Pong C++/Pong C++/src/Player.cpp
+++ Pong C++/Pong C++/src/Player.cpp
@@ -17,16 +17,17 @@ void Player::Draw()
     // I shouldn't be using a hardcoded integer there (instead I should have a reference to the engine or a global variable, or smth).
     // Currently doesn't work, it only increases or decreases its size.
 
+    float center = (((yPos / 800.0f) * 2.0f) - 1.0f) * -1.0f;
+
     glBegin(GL_QUADS);
-    glVertex2f(1.0f, -0.1f * (800 / yPos));
-    glVertex2f(1.0f, 0.1f * (800 / yPos));
-    glVertex2f(0.98f, 0.1f * (800 / yPos));
-    glVertex2f(0.98f, -0.1f * (800 / yPos));
+    glVertex2f(1.0f, -0.1f + center);
+    glVertex2f(1.0f, 0.1f + center);
+    glVertex2f(0.98f, 0.1f + center);
+    glVertex2f(0.98f, -0.1f + center);
     glEnd();
 }
 
 void Player::Move(double yPos)
 {
     this->yPos = yPos;
-    std::cout << yPos << std::endl;
 }
